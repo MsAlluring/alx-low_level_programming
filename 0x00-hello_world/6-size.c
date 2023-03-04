0090c107f24a9f97dbcb73c96b2b4172699582bf
Super User <Goodness Oriaku>
2023-03-03 23:48:06 -0800

    Size of a type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,10 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-printf("Size of a char: %1u byte(s)\n", (unsigned long)'size of(a));
-printf("Size of an int: %1u byte(s)\n", (unsigned long)'size of (b));
-printf("Size of a long int: %1u byte(s)\n", (unsigned long)'size of (c));
-printf("Size of a long long int: %1u byte(s)\n", (unsigned long)'size of (D));
+printf("Size of a char: %1u byte(s)\n", (unsigned long)size of(a));
+printf("Size of an int: %1u byte(s)\n", (unsigned long)size of(b));
+printf("Size of a long int: %1u byte(s)\n", (unsigned long)size of(c));
+printf("Size of a long long int: %1u byte(s)\n", (unsigned long)size of(d));
+printf("Size of a float: %1u byte(s)\n", (unsigned long)size of(f));
+return (0);
+}
