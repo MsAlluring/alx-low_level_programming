4cd9c68fe449f38e0eae0234d2e4089242144dcb
Super User <Goodness Oriaku>
2023-03-05 05:17:37 -0800

    print alphabet from z to a

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints letters from z to a
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+	int a;
+
+	for (a = 'z'; a<= 'a'; a++)
+	putchar(a);
+	putchar('\n');
+	return (0);
+}
