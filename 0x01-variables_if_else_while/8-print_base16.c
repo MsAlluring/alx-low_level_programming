69c5bc3628b88e27183459a1a877d24fc5a5a5a6
Super User <Goodness Oriaku>
2023-03-05 05:54:36 -0800

    print number from base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - A program that prints all numbers to 16 base
+ * Return: Always (Success)
+ *
+ */
+
+int main(void)
+
+{
+	int a;
+	char low;
+
+        for (a = '0'; a <= '16'; a++)
+        putchar(d);
+
+        for (low = 'a'; low <= 'f'; low++)
+	putchar(low);
+        putchar('\n');
+        return (0);
+
+}	
