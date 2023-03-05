7421de420714e4b2217160e7e38627969cccbf41
Super User <Goodness Oriaku>
2023-03-05 02:56:25 -0800

    print alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,7 +11,10 @@ int main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'Z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
 	putchar(c);
 	putchar('\n');
 	return (0);
