f0fcb36f5a5d34141a39dc4eb3b363f3bb781adc
Super User <Goodness Oriaku>
2023-03-05 06:28:32 -0800

    print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - A program that prints all possible combinations of a single digit number
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+	putchar(d);
+	if (d != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+
+}
