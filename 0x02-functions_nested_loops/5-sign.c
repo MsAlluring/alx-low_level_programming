09ce804752d811cfc38c838b5624cffb70c8adf3
Super User <Goodness Oriaku>
2023-03-05 14:04:46 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * abs - a function that prints the sign of a number
+ * @c: the number to be computed
+ * Return: absolute valus or zero
+ */
+
+int print_sign(int n)
+
+{
+	if (c < 0)
+	{
+	int abs_val;
+
+	abs_val = c * -1;
+	return (abs_val);
+	}
+	return (c);
+}
